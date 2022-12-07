d30777f5e2c987e0b3865ef658b30c8e49cba271
Yong <yonghwan311@gmail.com>
2022-12-07 03:34:02 -0700

    revise skill

--- skill/skill.js
+++ skill/skill.js
@@ -16,20 +16,28 @@ getSkill(requestURL);
 function displaySkill(item) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let h4= document.createElement('h4');
+    let h41= document.createElement('h4');
+    let h42= document.createElement('h4');
+    let h43= document.createElement('h4');
+    let h44= document.createElement('h4');
+    let h45= document.createElement('h4');
     let img = document.createElement('img');
 
     img.setAttribute('src', item.image);
     h2.textContent = item.name;
-    h4.textContent = 'I have Project experience with ' + item.name + ' : ' + item.made
-    h4.textContent = 'Take course : '
-    h4.textContent = '    ' + item.class1;
-    h4.textContent = '    ' + item.class2;
-    h4.textContent = 'Start Learning year : ' + item.year;
+    h41.textContent = 'I have Project experience with ' + item.name + ' : ' + item.made
+    h42.textContent = 'Take course : '
+    h43.textContent = '    ' + item.class1;
+    h44.textContent = '    ' + item.class2;
+    h45.textContent = 'Start Learning year : ' + item.year;
     
     card.appendChild(img);
     card.appendChild(h2);
-    card.appendChild(h4);
+    card.appendChild(h41);
+    card.appendChild(h42);
+    card.appendChild(h43);
+    card.appendChild(h44);
+    card.appendChild(h45);
     
     document.querySelector(".cards").appendChild(card);
 }
